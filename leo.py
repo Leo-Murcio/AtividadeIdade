b0c7d496cb1e6e6a7fb715a90bcb56da55616ece
Leonardo <leo.moleiromurcio@gmail.com>
2023-03-05 13:22:55 -0300

    AtividadeIdade

---
+++ leo.py
@@ -0,0 +1,16 @@
+#AtividadeLeo
+
+nome = "Leonardo Moleiro Murcio"
+peso = "75kg"
+altura = "1 metro e 77 centimetros"
+idade = 20
+
+print (nome)
+print (peso)
+print (altura)
+print (str(idade) + " anos")
+
+if idade >= 18:
+    print ("Sim, sou maior de idade!")
+else:
+    print ("Não, ainda não sou maior de idade!")
